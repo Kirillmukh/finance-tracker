512a8ed4cb441479052d2f8259986a49dcfd5c8f
kirillmukh <kirillmukh@mail.ru>
2026-06-12 13:30:11 +0300

    fix: less margin between slider and bottom border

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump VERSION whenever app shell files change to invalidate clients' caches.
-const VERSION = 'v15';
+const VERSION = 'v16';
 const CACHE_NAME = `finance-tracker-${VERSION}`;
 
 const APP_SHELL = [
